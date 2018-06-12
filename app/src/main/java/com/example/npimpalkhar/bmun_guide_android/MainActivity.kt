ddebf1d9e7e1db0a63b93f0819504ad2f4ea6e98
Nikhil Pimpalkhare <Nikhil_Pimpalkhare@intuit.com>
2018-06-12 14:33:05 -0700

    Some fundamental structure

--- app/src/main/java/com/example/npimpalkhar/bmun_guide_android/MainActivity.kt
+++ app/src/main/java/com/example/npimpalkhar/bmun_guide_android/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.npimpalkhar.bmun_guide_android
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NavigationHost {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.container, DGuideFragment())
+                    .commit()
+        }
     }
+
+    override fun navigateTo(fragment: Fragment, addToBackstack: Boolean) {
+        val transaction = supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+        if (addToBackstack) {
+            transaction.addToBackStack(null)
+        }
+
+        transaction.commit()
+    }
+
 }
